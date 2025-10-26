e1be9d1e8d225c4707e1c3830f2fe33c0522300d
saviachalack5-ctrl <savia.chalack5@gmail.com>
2025-10-26 16:32:40 +0300

    Login.jsx

--- login/src/Login.jsx
+++ login/src/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./App.css"; // Make sure this CSS file is in the same folder
+import "./App.css"; 
 
 const Login = ({ error, setError }) => {
   const navigate = useNavigate();
