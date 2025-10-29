ba9a2fbdc155914d478bcf7d5066310f163bc3ef
saviachalack5-ctrl <savia.chalack5@gmail.com>
2025-10-29 11:23:59 +0300

    SideBar.jsx

--- login/src/SideBar.jsx
+++ login/src/SideBar.jsx
@@ -1,58 +1,25 @@
 import React from "react";
+import './App.css'
 
 const SideBar = () => {
-  const sidebarStyle = {
-    width: "250px",
-    height: "100vh",
-    backgroundColor: "#1e1e2f",
-    color: "white",
-    padding: "20px",
-    boxSizing: "border-box",
-    position: "fixed",
-    top: 0,
-    right: 0, // or 'left: 0' if you want it on the left side
-    zIndex: 9999, // makes sure it’s above other elements
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "flex-start",
-    alignItems: "flex-start",
-  };
 
-  const listStyle = {
-    listStyle: "none",
-    padding: 0,
-    margin: 0,
-    width: "100%",
-  };
-
-  const linkStyle = {
-    color: "white",
-    textDecoration: "none",
-    display: "block",
-    padding: "10px 0",
-    width: "100%",
-  };
-
-  const linkHover = {
-    textDecoration: "underline",
-  };
 
   return (
-    <aside style={sidebarStyle}>
+    <aside>
       <nav>
-        <ul style={listStyle}>
+        <ul className="Side-bar">
           <li>
-            <a href="#" style={linkStyle}>
+            <a className="SideBar-link" href="#">
               Profile
             </a>
           </li>
           <li>
-            <a href="#" style={linkStyle}>
+            <a className="SideBar-link" href="#">
               Users
             </a>
           </li>
           <li>
-            <a href="#" style={linkStyle}>
+            <a className="SideBar-link" href="#">
               Archives
             </a>
           </li>
